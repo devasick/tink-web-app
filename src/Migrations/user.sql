8efac4d5e8f23cb5a60b15a2d71ae4621698ae1e
Asick Ahamed <ashiq.it@gmail.com>
2019-09-15 17:40:09 +0200

    Update user.sql

--- src/Migrations/user.sql
+++ src/Migrations/user.sql
@@ -32,6 +32,9 @@ CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(180) COLLATE utf8mb4_unicode_ci NOT NULL,
   `roles` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL
+   `password` varchar(180) utf8mb4_unicode_ci NOT NULL,
+   `salt` varchar(180) utf8mb4_unicode_ci NOT NULL,
+   `is_active` int(11) utf8mb4_unicode_ci NOT NULL,
 ) ;
 
 --
